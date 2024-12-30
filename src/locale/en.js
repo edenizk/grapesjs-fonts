5766668e9494452c735beaf622921c9ab885e852
Ergin Deniz Kosecioglu <ergindenizkosecioglu@gmail.com>
2024-12-30 21:59:53 +0100

    Update placeholder anme

--- src/locale/en.js
+++ src/locale/en.js
@@ -2,7 +2,7 @@ export default {
     'grapesjs-fonts': {
         'Fonts': 'Fonts',
         'You must provide Google font api key': 'You must provide Google font api key, see https://developers.google.com/fonts/docs/developer_api#APIKey',
-        'Search': 'Search fonts...',
+        'Search': 'Enter your font name or Search fonts...',
         'Add font': 'Install font',
         'Add Custom font': 'Create custom font',
         'Remove': 'Remove',
